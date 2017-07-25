7821615d2d6d2e9c8418d9f6ec761120c49fe5b1
Justin Guidroz <justin.guidroz@gmail.com>
2017-07-25 16:36:26 -0700

    Add drop table statement. Update created_on to TIMESTAMPTZ

--- db/users.sql
+++ db/users.sql
@@ -1,8 +1,10 @@
+DROP TABLE IF EXISTS users;
+
 CREATE TABLE users (
 		user_id BYTES PRIMARY KEY DEFAULT uuid_v4(),
 		username STRING UNIQUE NOT NULL,
 		salt BYTES NOT NULL,
 		password BYTES NOT NULL,
 		active BOOL DEFAULT true,
-		created_on TIMESTAMP NOT NULL
+		created_on TIMESTAMPTZ NOT NULL
 );
