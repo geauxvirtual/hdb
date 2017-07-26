25f838293ed54ea2a6823abe365e79dc8d93a685
Justin Guidroz <justin.guidroz@gmail.com>
2017-07-26 14:56:23 -0700

    Make fields public

--- src/platform/models/users.rs
+++ src/platform/models/users.rs
@@ -13,12 +13,12 @@ pub struct NewUser {
 
 #[derive(Debug)]
 pub struct User {
-    id: Vec<u8>,
-    username: String,
-    salt: Vec<u8>,
-    password: Vec<u8>,
-    active: bool,
-    created_on: DateTime<Utc>,
+    pub id: Vec<u8>,
+    pub username: String,
+    pub salt: Vec<u8>,
+    pub password: Vec<u8>,
+    pub active: bool,
+    pub created_on: DateTime<Utc>,
 }
 
 impl User {
@@ -64,15 +64,15 @@ pub fn get_by_username(username: &str, conn: &Connection) -> Result<User, &'stat
         .prepare("SELECT * FROM users WHERE username = $1;")
         .unwrap();
     for row in &stmt.query(&[&username]).unwrap() {
-        let user = User {
-            id: row.get(0),
-            username: row.get(1),
-            salt: row.get(2),
-            password: row.get(3),
-            active: row.get(4),
-            created_on: row.get(5),
-        };
-        return Ok(user);
+        return Ok(
+            User {
+                id: row.get(0),
+                username: row.get(1),
+                salt: row.get(2),
+                password: row.get(3),
+                active: row.get(4),
+                created_on: row.get(5),
+            })
     }
     Err("User not found")
 }
