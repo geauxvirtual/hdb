df2fa803b5a830e4d04075e792cf45c528384d7b
Justin Guidroz <justin.guidroz@gmail.com>
2017-08-02 14:39:05 -0700

    Remove expires column from tokens.sql script

--- db/tokens.sql
+++ db/tokens.sql
@@ -3,6 +3,5 @@ DROP TABLE IF EXISTS user_tokens;
 CREATE TABLE user_tokens (
 		id SERIAL PRIMARY KEY,
 		user_id BYTES UNIQUE NOT NULL REFERENCES users (user_id),
-		token BYTES UNIQUE NOT NULL,
-		expires TIMESTAMPTZ NOT NULL
+		token BYTES UNIQUE NOT NULL
 );
